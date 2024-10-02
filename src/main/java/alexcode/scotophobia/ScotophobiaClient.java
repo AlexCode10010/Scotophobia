ace97a71aa1e312cf1dbfc7e04dc74c0c0d7e3e2
AlexCode <unconfigured@null.spigotmc.org>
2024-10-02 17:12:42 +0200

    Initial commit

---
+++ src/main/java/alexcode/scotophobia/ScotophobiaClient.java
@@ -0,0 +1,10 @@
+package alexcode.scotophobia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScotophobiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/alexcode/scotophobia/ScotophobiaDataGenerator.java
@@ -0,0 +1,11 @@
+package alexcode.scotophobia;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ScotophobiaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
